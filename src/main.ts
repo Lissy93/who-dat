4f61d57c176288b25fde685837ae0ca83c2d1953
Alicia Sykes <alicia@omg.lol>
2024-01-09 11:43:05 +0000

    Show results in tabular format on web UI

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,28 @@ import './styles/document.scss'
 import './styles/layout.scss'
 import './styles/form.scss'
 import './styles/content.scss'
+import './styles/results.scss'
 
 import Alpine from 'alpinejs'
 
+interface FormattedResult {
+  lbl: string;
+  val: string | FormattedResult[];
+};
+
+
 function whoisLookup() {
   return {
-      domain: '',
-      result: '',
-      error: '',
-      lookup() {
-        // fetch(`https://potential-bassoon-77r4qqr57g2x5xr-3000.app.github.dev/${this.domain}`)
-        fetch(`/${this.domain}`)
+    domain: '',
+    result: '',
+    error: '',
+    formattedResult: [] as FormattedResult[],
+    showFormatted: true,
+    toggleFormat() {
+      this.showFormatted = !this.showFormatted;
+    },
+    lookup() {
+      fetch(`/${this.domain}`)
         .then(response => {
           if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -24,16 +35,34 @@ function whoisLookup() {
         })
         .then(data => {
           this.result = JSON.stringify(data, null, 2);
+          this.formattedResult = this.formatResult(data);
           this.error = '';
+          console.log(this.formatResult(data));
         })
         .catch(error => {
           this.error = 'Failed to fetch WHOIS data';
           this.result = '';
+          this.formattedResult = [];
           console.error('There has been a problem with your fetch operation:', error);
         });
-      }
+    },
+    formatResult(data: any) {
+      const result: FormattedResult[] = [];
+      Object.keys(data).forEach(key => {
+        const sectionResults: FormattedResult[] = [];
+        const label = key.replace(/_/g, ' ');
+        const values = data[key];
+        Object.keys(values).forEach(lbl => {
+          const val = values[lbl];
+          sectionResults.push({lbl, val});
+        });
+        result.push({ lbl: label, val: sectionResults });
+      });
+      return result;
+    }
   };
 }
+
 
 Alpine.data('whoisLookup', whoisLookup);
 
